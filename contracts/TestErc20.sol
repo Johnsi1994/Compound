e7c6d229768c85e3dab356228b63c453e222d922
Johnson-OpNet <johnson.tu@opennet.tw>
2022-11-04 15:12:08 +0800

    refactor week 11 testing

--- contracts/TestErc20.sol
+++ contracts/TestErc20.sol
@@ -5,8 +5,12 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
-
-    constructor(uint _supply) ERC20("MyToken", "MTK") {
+    
+    constructor(
+        string memory _name,
+        string memory _symble,
+        uint256 _supply
+    ) ERC20(_name, _symble) {
         _mint(msg.sender, _supply);
     }
 
--- test/Compound.js
+++ test/Compound.js
@@ -3,78 +3,94 @@ const { ethers } = require("hardhat");
 
 describe("Compound", function () {
     const MINT_AMOUNT = 100n * 10n ** 18n;
+    let owner, signer1
+    let erc20A, cErc20A, comptroller, simplePriceOracle, interestRateModel
 
-    it("Test", async function () {
-        // get admin address
-        const [owner, otherAccount] = await ethers.getSigners();
+    it("get signers", async function () {
+        [owner, signer1] = await ethers.getSigners();
+    })
 
-        // deploy underlying asset
-        const erc20Factory = await ethers.getContractFactory("TestERC20");
-        const testErc20 = await erc20Factory.deploy(ethers.utils.parseUnits("1000000", 18));
-        await testErc20.deployed();
+    describe("deploy", async () => {
+        it("deploy erc20A", async function () {
+            const erc20Factory = await ethers.getContractFactory("TestERC20");
+            erc20A = await erc20Factory.deploy(
+                "TokenA",
+                "ATK",
+                ethers.utils.parseUnits("1000000", 18)
+            );
+            await erc20A.deployed();
+        })
 
-        // deploy comptroller
-        const comptrollerFactory = await ethers.getContractFactory("Comptroller");
-        const comptroller = await comptrollerFactory.deploy();
-        await comptroller.deployed();
+        it("deploy comptroller", async function () {
+            const comptrollerFactory = await ethers.getContractFactory("Comptroller");
+            comptroller = await comptrollerFactory.deploy();
+            await comptroller.deployed();
+        })
 
-        // deploy priceOracle
-        const priceOracleFactory = await ethers.getContractFactory("SimplePriceOracle");
-        const simplePriceOracle = await priceOracleFactory.deploy();
-        await simplePriceOracle.deployed();
+        it("deploy simplePriceOracle", async function () {
+            const priceOracleFactory = await ethers.getContractFactory("SimplePriceOracle");
+            simplePriceOracle = await priceOracleFactory.deploy();
+            await simplePriceOracle.deployed();
+        })
 
-        // setup SimplePriceOracle to Comptroller
-        comptroller._setPriceOracle(simplePriceOracle.address);
+        it("deploy interest rate model", async function () {
+            const interestRateModelFactory = await ethers.getContractFactory("WhitePaperInterestRateModel");
+            interestRateModel = await interestRateModelFactory.deploy(
+                ethers.utils.parseUnits("0", 18),
+                ethers.utils.parseUnits("0", 18),
+            );
+            await interestRateModel.deployed();
+        })
 
-        // deploy interest rate model
-        const interestRateFactory = await ethers.getContractFactory("WhitePaperInterestRateModel");
-        const interestRate = await interestRateFactory.deploy(
-            ethers.utils.parseUnits("0", 18),
-            ethers.utils.parseUnits("0", 18),
-        );
-        await interestRate.deployed();
+        it("deploy cErc20A", async function () {
+            const cErc20Factory = await ethers.getContractFactory("CErc20Immutable");
+            cErc20A = await cErc20Factory.deploy(
+                erc20A.address,
+                comptroller.address,
+                interestRateModel.address,
+                ethers.utils.parseUnits("1", 18),
+                "Test Token",
+                "TTK",
+                18,
+                owner.address,
+            );
+            await cErc20A.deployed();
+        })
+    })
 
-        // deploy cErc20
-        const cErc20Factory = await ethers.getContractFactory("CErc20Immutable");
-        const cErc20 = await cErc20Factory.deploy(
-            testErc20.address,
-            comptroller.address,
-            interestRate.address,
-            ethers.utils.parseUnits("1", 18),
-            "Test Token",
-            "TTK",
-            18,
-            owner.address,
-        );
-        await cErc20.deployed();
+    describe("Settings", async () => {
+        it("setup SimplePriceOracle to Comptroller", async function () {
+            await comptroller._setPriceOracle(simplePriceOracle.address);
+        })
 
-        // setup supportMarket to Comptroller
-        comptroller._supportMarket(cErc20.address);
+        it("setup supportMarket to Comptroller", async function () {
+            await comptroller._supportMarket(cErc20A.address);
+        })
+    })
 
+    describe("Test mint and redeem", async () => {
+        it("Test mint", async function () {
+            // approve cErc20A address and mint
+            await erc20A.approve(cErc20A.address, MINT_AMOUNT);
+            await cErc20A.mint(MINT_AMOUNT);
 
+            // cErc20's erc20A increase
+            expect(await erc20A.balanceOf(cErc20A.address)).to.equal(MINT_AMOUNT);
 
-        // test mint
-        await testErc20.approve(cErc20.address, MINT_AMOUNT);
-        await cErc20.mint(MINT_AMOUNT);
+            // owner's cErc20A increase
+            expect(await cErc20A.balanceOf(owner.address)).to.equal(MINT_AMOUNT);
+        });
 
-        // cErc20's testErc20 increase
-        expect(await testErc20.balanceOf(cErc20.address)).to.equal(MINT_AMOUNT);
+        it("Test redeem", async function () {
+            // redeem
+            await cErc20A.redeem(MINT_AMOUNT);
 
-        // owner's cErc20 increase
-        expect(await cErc20.balanceOf(owner.address)).to.equal(MINT_AMOUNT);
+            // cErc20's erc20A decrease
+            expect(await erc20A.balanceOf(cErc20A.address)).to.equal(0);
 
+            // owner's cErc20A decrease
+            expect(await cErc20A.balanceOf(owner.address)).to.equal(0);
+        });
+    })
 
-
-        // test redeem
-        await cErc20.approve(owner.address, MINT_AMOUNT);
-        await cErc20.redeem(MINT_AMOUNT);
-
-        // cErc20's testErc20 decrease
-        expect(await testErc20.balanceOf(cErc20.address)).to.equal(0);
-
-        // owner's cErc20 decrease
-        expect(await cErc20.balanceOf(owner.address)).to.equal(0);
-
-        // changeTokenBalances : do research
-    });
 });
